59584331811799d8eb849c4d6444c571adea74f9
Damodar Dahal <underscoredam@gmail.com>
2017-03-04 12:08:45 -0600

    integrate config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+PORT = 9090
+
+DEBUG = True
+
+HOST = "0.0.0.0"
+
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import connexion 
+import config
+from flask_cors import CORS
 
 if __name__ == "__main__": 
-    app = connexion.App(__name__, 9090, specification_dir='swagger/')
+    app = connexion.App(__name__, debug=config.DEBUG, host=config.HOST, port=config.PORT, specification_dir='swagger/')
+    CORS(app.app)
     app.add_api('api.yaml')
     app.run()
