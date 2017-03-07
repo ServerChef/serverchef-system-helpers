bd579068cd32788dfa6984913528d4554d7ada83
Damodar Dahal <underscoredam@gmail.com>
2017-03-07 13:59:10 -0600

    move app declaration out of __name__

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import connexion
 import config
 from flask_cors import CORS
 
+app = connexion.App(__name__, debug=config.DEBUG, host=config.HOST, port=config.PORT, specification_dir='swagger/')
+CORS(app.app)
+app.add_api('api.yaml')
 if __name__ == "__main__": 
-    app = connexion.App(__name__, debug=config.DEBUG, host=config.HOST, port=config.PORT, specification_dir='swagger/')
-    CORS(app.app)
-    app.add_api('api.yaml')
     app.run()
